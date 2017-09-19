5fd9abbd5fcc58e15c09b8ebb3ecf0c1bc5295a6
DAIRE NI CHATHAIN - Student <G00334757@gmit.ie>
2017-09-19 14:51:07 +0100

    My first commit

---
+++ 01_konnichiwa_sekai.go
@@ -0,0 +1,10 @@
+//From: https://tour.golang.org/welcome/1
+//Author: Daire Ní Chatháin
+//Problem sheet 1 : Kon'nichiwa, Sekai!
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#Answers to Problem sheet 1
