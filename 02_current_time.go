e84a48eb7839b2a649de28b87562922f4ca27650
Daire Ní Chatháin <dairenichat@gmail.com>
2017-09-19 15:34:57 +0100

    Added exercise 2

---
+++ 02_current_time.go
@@ -0,0 +1,15 @@
+//From : 
+//Author: Daire Ní Chatháin
+//Problem sheet 1 : Current Time
+//This program prints the current time
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello, the time is", time.Now())
+}
