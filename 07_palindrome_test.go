de6d1ea85dceebde713089641a784ba13080aab7
Daire Ni Chathain <dairenichat@gmail.com>
2017-09-24 13:34:53 +0100

    updated more efficient solution to plaindrome tester

--- 07_palindrome_test.go
+++ 07_palindrome_test.go
@@ -1,7 +1,8 @@
-//Author: Daire Ní Chatháin
-//Problem sheet 1 : Palindrome test
-/* This Program that tests whether a string is a palindrome.
-Adapted from: 
+// Author: Daire Ní Chatháin
+// Problem sheet 1 : Palindrome test
+/* This Program allows the user to enter and string and
+tests whether that string is a palindrome or not.
+Adapted from: https://gist.github.com/renandanton/e99eb16f46e3decceca59b3dbe9f0bb4
 */
 package main
 
@@ -9,23 +10,29 @@ import "fmt"
 
 func main() {
 
-	var word string = ""
-	var isPalindrome = False
+	var word string
+	var isPalindrome = false
+
 	fmt.Println("=== Palindrome Tester ===")
 	fmt.Println("Enter a word: ")
 
-	//input of word
-
-    fmt.Scanln(&word)
+	//User input word
+	fmt.Scanln(&word)
 	length := len(word)
-
-	for i:=0 ; i < (length/2) ; i++ {
-		if string(word[i]) != string(word[length - i -1]){
-
-		}else{
+	//check if each half of word are identical
+	for i := 0; i < (length / 2); i++ {
+		if string(word[i]) != string(word[length-1-i]) {
+			isPalindrome = false
+		} else {
 			isPalindrome = true
 		}
-	
+	}
+
+	// Print result
+	if isPalindrome {
+		fmt.Println("Your word is a palindrome")
+	} else {
+		fmt.Println("Your word is  not a palindrome")
 	}
 
 }
