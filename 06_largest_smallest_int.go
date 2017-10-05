30006873dea7c0268451ecb748de2a915775d876
DaireNiC <dairenichat@gmail.com>
2017-10-05 19:14:01 +0100

    Added comments

--- 06_largest_smallest_int.go
+++ 06_largest_smallest_int.go
@@ -1,16 +1,27 @@
 //Author: Daire Ní Chatháin
 //Problem sheet 1 : Largest and Smallest in Int
+
 /* This Program that returns the largest  and smallest elements in a list.
-Adapted from : https://golang.org/pkg/sort/
+
+Resource: https://golang.org/pkg/sort/
+
 */
+
 package main
 
 import "fmt"
 import "sort"
 
 func main() {
-	s := []int{5, 2, 6, 3, 1, 4} // unsorted
+	
+	//an unsorted array of integers
+	s := []int{5, 2, 6, 3, 1, 4}
+	//print unsorted values
 	fmt.Println("Unsorted : ", s)
+	
+	//sort using inbuilt sort method for ints
 	sort.Ints(s)
+	
+	//print sorted result to console 
 	fmt.Println("Sorted: ", s)
 }
