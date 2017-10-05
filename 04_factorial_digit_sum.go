05a63bcbd1c434ab7b3d5986dcfaaae2559c310f
DaireNiC <dairenichat@gmail.com>
2017-10-05 19:11:23 +0100

    Added comments to f04_factorial_digit_sum

--- 04_factorial_digit_sum.go
+++ 04_factorial_digit_sum.go
@@ -1,7 +1,7 @@
 //Author: Daire Ní Chatháin
 //Problem sheet 1 : Factorial Digit Sum
-/* A  program that calculates the sum of the digits
- of the factorial of a number 
+/* A  program that calculates factorial of a number & the sum of the digits
+ of that number
  */
 package main
 
@@ -13,17 +13,21 @@ func main() {
 	var factorial int = 1
 	
 	for i := 0; i < num; i++{
-		
+		//calculate factorial
 		factorial += factorial * i 
 	}
-
+	//print factorial
 	fmt.Println(factorial)
 
 	var digit, sum int =  0,0;
-
+	
+	//while there are  are more than 0 digits in the number
 	for factorial > 0 {
+		//split num by using modulo to separate digits
 		digit = factorial  % 10
+		//add digit to sum
 		sum += digit
+		//update new factorial value (one digit less each time)
 		factorial /=10
 	}
 	fmt.Println(sum)
